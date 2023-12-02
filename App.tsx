a58d1d17a83729c72aaae15ff7ac29e4835d4c60
Tasheem <thargro1@uab.edu>
2023-12-01 18:35:03 -0600

    Check to make sure debugging is working in VSCode by using attach to Hermes app configuration

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const doSomething = () => {
+    const num1 = 5;
+    const num2 = 7;
+    const result = num1 + num2;
+    console.log(`Result: ${result}`);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title='Press Me' onPress={doSomething} />
       <StatusBar style="auto" />
     </View>
   );
