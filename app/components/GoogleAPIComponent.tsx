00a7485170915151107086aaf55810238fbf1acb
Tasheem <thargro1@uab.edu>
2023-12-05 10:29:33 -0600

    Create function to format phone numbers

--- app/components/GoogleAPIComponent.tsx
+++ app/components/GoogleAPIComponent.tsx
@@ -102,15 +102,15 @@ const renderResultsList = (businesses: Business[]) => {
 							<Text>{item.name}</Text>
 						</View>
 
-						{/* <View style={resultsStyle.detailsRow}>
+						<View style={resultsStyle.detailsRow}>
 							<Text style={resultsStyle.label}>Address:</Text>
-							<Text>{item.location}</Text>
-						</View> */}
+							<Text>{`${item.location.address1}, ${item.location.city}, ${item.location.state}`}</Text>
+						</View>
 
-						{/* <View style={resultsStyle.detailsRow}>
+						<View style={resultsStyle.detailsRow}>
 							<Text style={resultsStyle.label}>Type:</Text>
-							<Text>{sectionData.item.type}</Text>
-						</View> */}
+							<Text>{formatPhoneNumber(item.phone)}</Text>
+						</View>
 					</View>
 					<BouncyCheckbox
 						size={25}
@@ -124,6 +124,18 @@ const renderResultsList = (businesses: Business[]) => {
 			)}
 		/>
 	)
+}
+
+const formatPhoneNumber = (pn: string) => {
+	if(!pn) {
+		return '';
+	}
+
+	const phoneNumber = pn.slice(2);
+	const split = [phoneNumber.slice(0, 3), phoneNumber.slice(3, 6), phoneNumber.slice(6, 10)];
+	const formattedNumber = `${split[0]}-${split[1]}-${split[2]}`;
+
+	return formattedNumber;
 }
 
 const styles = StyleSheet.create({
