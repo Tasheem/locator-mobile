9fd23492e1588b0ea0bea5ea94014ef677aa70e2
Tasheem <thargro1@uab.edu>
2023-12-05 18:09:23 -0600

    Fix bug where API breaks when user enters a searchTerm with a space in it

--- app/services/fetchYelpRestaurants.ts
+++ app/services/fetchYelpRestaurants.ts
@@ -2,6 +2,7 @@ import { Business } from "../models/yelp-api";
 
 //fetches restaurants based on searchTerm. i.e: 'Vegan', 'Vegetarian', etc.
 export function fetchYelpRestaurants(searchTerm: string, apiKey: string) {
+	searchTerm = searchTerm.replace(" ", "%20");
 	const url = `https://api.yelp.com/v3/businesses/search?latitude=33.5186&longitude=-86.8104&term=${searchTerm}%20food&categories=&sort_by=best_match&limit=20`;
 
 	const headers = {
