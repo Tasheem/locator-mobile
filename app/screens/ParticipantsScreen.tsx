267c3d72e5ff71295c58278a55b13e53653e7c76
Tasheem <thargro1@uab.edu>
2024-08-26 23:26:09 -0500

    Add tabs to the Room Details screen and create new components

---
+++ app/screens/ParticipantsScreen.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export default function ParticipantsScreen() {
+    return (
+        <View>
+            
+        </View>
+    )
+}
--- app/screens/RoomDetailsScreen.tsx
+++ app/screens/RoomDetailsScreen.tsx
@@ -1,11 +1,16 @@
-import { SafeAreaView, View } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import ParticipantsScreen from "./ParticipantsScreen";
+import ChatScreen from "./ChatScreen";
+import RecommendationScreen from "./RecommendationScreen";
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function RoomDetailsScreen() {
     return (
-        <SafeAreaView>
-            <View>
-                
-            </View>
-        </SafeAreaView>
+        <Tab.Navigator>
+            {/* <Tab.Screen name="Participants" component={ ParticipantsScreen } /> */}
+            <Tab.Screen name="Chat" component={ ChatScreen } />
+            <Tab.Screen name="Recommendation" component={ RecommendationScreen } />
+        </Tab.Navigator>
     );
 }
