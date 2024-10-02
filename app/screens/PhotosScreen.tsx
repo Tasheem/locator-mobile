054de8af618a5f488c47767d90eeb32d377010f8
Tasheem <thargro1@uab.edu>
2024-10-02 16:19:56 -0500

    Display larger version of image when an image is pressed

--- app/screens/PhotosScreen.tsx
+++ app/screens/PhotosScreen.tsx
@@ -1,9 +1,10 @@
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RouteProp } from "@react-navigation/native";
 import { RootStackParamList } from "../../App";
-import { FlatList, SafeAreaView, Image, StyleSheet, View, Dimensions } from "react-native";
+import { SafeAreaView, Image, StyleSheet, View, Dimensions, Modal } from "react-native";
 import { useEffect, useState } from "react";
 import { fetchUserImages } from "../services/media-service";
+import { TouchableOpacity, TouchableWithoutFeedback } from "react-native-gesture-handler";
 
 type Props = {
     navigation: NativeStackNavigationProp<
@@ -16,6 +17,8 @@ type Props = {
 
 export default function PhotosScreen({}: Props) {
     const [photoUrls, setPhotoUrls] = useState<LocatorImageData[]>([]);
+    const [enlargedPhotoVisible, setEnlargedPhotoVisible] = useState(false);
+    const [photoInFocus, setPhotoInFocus] = useState<LocatorImageData | null>(null);
 
     useEffect(() => {
         fetchUserImages()
@@ -23,6 +26,8 @@ export default function PhotosScreen({}: Props) {
             if(response.ok) {
                 return response.json();
             }
+
+            return Promise.resolve([]);
         })
         .then((urls: LocatorImageData[]) => {
             setPhotoUrls(urls);
@@ -35,32 +40,66 @@ export default function PhotosScreen({}: Props) {
 
         return (
             <View style={style.imageContainer}>
-                <Image key={imageData.publicUrl}
-                    source={{ uri: imageData.publicUrl }}
-                    style={[style.image, {
-                        marginLeft: startItem ? 0 : 1,
-                        marginRight: endItem ? 0 : 1,
-                        alignSelf: endItem ? 'flex-end' : 'auto'
-                    }]}
-                />
+                <TouchableOpacity
+                    key={imageData.publicUrl}
+                    onPress={() => {
+                        setPhotoInFocus(imageData);
+                        setEnlargedPhotoVisible(true);
+                    }}
+                >
+                    <Image
+                        source={{ uri: imageData.publicUrl }}
+                        style={[style.image, {
+                            marginLeft: startItem ? 0 : 1,
+                            marginRight: endItem ? 0 : 1
+                        }]}
+                    />
+                </TouchableOpacity>
             </View>
         );
     });
 
     return (
         <SafeAreaView style={{
-            height: '100%',
-            alignItems: 'center'
+            height: '100%'
         }}>
             <View style={style.listContainer}>
                 { renderedPhotos }
             </View>
+
+            <Modal
+                animationType='fade'
+                visible={enlargedPhotoVisible}
+                transparent={true}
+                onRequestClose={() => {
+                    setEnlargedPhotoVisible(false);
+                }}
+            >
+                <SafeAreaView style={{
+                    backgroundColor: 'rgba(0, 0, 0, 0.9)'
+                }}>
+                    <TouchableOpacity
+                        style={modalStyle.rootContainer}
+                        onPress={() => {
+                            setEnlargedPhotoVisible(false);
+                            setPhotoInFocus(null);
+                        }}
+                    >
+                        <TouchableWithoutFeedback>
+                            <Image
+                                source={{ uri: photoInFocus?.publicUrl }}
+                                style={modalStyle.image}
+                            />
+                        </TouchableWithoutFeedback>
+                    </TouchableOpacity>
+                </SafeAreaView>
+            </Modal>
         </SafeAreaView>
     );
 }
 
-console.log(`Screen width: ${Dimensions.get('window').width}`);
-console.log(`Image width: ${(Dimensions.get('window').width / 4) - 1.5}`);
+/* console.log(`Screen width: ${Dimensions.get('window').width}`);
+console.log(`Image width: ${(Dimensions.get('window').width / 4) - 1.5}`); */
 
 const style = StyleSheet.create({
     listContainer: {
@@ -76,4 +115,17 @@ const style = StyleSheet.create({
         width: (Dimensions.get('window').width / 4) - 1.5,
         height: (Dimensions.get('window').width / 4) - 1.5
     }
-})
+});
+
+const modalStyle = StyleSheet.create({
+    rootContainer: {
+        height: '100%',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    image: {
+        width: Dimensions.get('window').width - 50,
+        height: Dimensions.get('window').height - 300
+    }
+});
