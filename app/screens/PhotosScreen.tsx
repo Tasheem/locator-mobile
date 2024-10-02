536e8c8fb26d12c782670026d44ede3b2fa5ee15
Tasheem <thargro1@uab.edu>
2024-10-02 16:54:30 -0500

    Add ability to swipe up or down on an enlarged image to close the modal

--- app/screens/PhotosScreen.tsx
+++ app/screens/PhotosScreen.tsx
@@ -1,10 +1,11 @@
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RouteProp } from "@react-navigation/native";
 import { RootStackParamList } from "../../App";
-import { SafeAreaView, Image, StyleSheet, View, Dimensions, Modal } from "react-native";
+import { SafeAreaView, Image, StyleSheet, Dimensions, Modal } from "react-native";
 import { useEffect, useState } from "react";
 import { fetchUserImages } from "../services/media-service";
-import { ScrollView, TouchableOpacity, TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { ScrollView, TouchableOpacity, TouchableWithoutFeedback, GestureDetector, Gesture, GestureHandlerRootView } from "react-native-gesture-handler";
+import Animated, { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
 
 type Props = {
     navigation: NativeStackNavigationProp<
@@ -15,10 +16,51 @@ type Props = {
     route: RouteProp<RootStackParamList, 'Photos'>;
 };
 
+function clamp(val: number, min: number, max: number) {
+    return Math.min(Math.max(val, min), max);
+}
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get('screen');
+
 export default function PhotosScreen({}: Props) {
     const [photoUrls, setPhotoUrls] = useState<LocatorImageData[]>([]);
     const [enlargedPhotoVisible, setEnlargedPhotoVisible] = useState(false);
     const [photoInFocus, setPhotoInFocus] = useState<LocatorImageData | null>(null);
+
+    const translationY = useSharedValue(0);
+    const prevTranslationY = useSharedValue(0);
+  
+    const animatedStyles = useAnimatedStyle(() => ({
+      transform: [
+        { translateY: translationY.value }
+      ],
+    }));
+
+    const pan = Gesture.Pan()
+    .minDistance(1)
+    .onStart(() => {
+      prevTranslationY.value = translationY.value;
+    })
+    .onUpdate((event) => {
+      const maxTranslateX = screenWidth / 2 - 50;
+      const maxTranslateY = screenHeight / 2 - 50;
+
+      translationY.value = clamp(
+        prevTranslationY.value + event.translationY,
+        -maxTranslateY,
+        maxTranslateY
+      );
+    })
+    .onEnd((event) => {
+        if(Math.abs(translationY.value) > 200) {
+            setPhotoInFocus(null);
+            setEnlargedPhotoVisible(false);
+        }
+
+        translationY.value = 0;
+        prevTranslationY.value = 0;
+    })
+    .runOnJS(true);
 
     useEffect(() => {
         fetchUserImages()
@@ -76,20 +118,26 @@ export default function PhotosScreen({}: Props) {
                 <SafeAreaView style={{
                     backgroundColor: 'rgba(0, 0, 0, 0.9)'
                 }}>
-                    <TouchableOpacity
-                        style={modalStyle.rootContainer}
-                        onPress={() => {
-                            setEnlargedPhotoVisible(false);
-                            setPhotoInFocus(null);
-                        }}
-                    >
-                        <TouchableWithoutFeedback>
-                            <Image
-                                source={{ uri: photoInFocus?.publicUrl }}
-                                style={modalStyle.image}
-                            />
-                        </TouchableWithoutFeedback>
-                    </TouchableOpacity>
+                    <GestureHandlerRootView>
+                        <GestureDetector gesture={pan}>
+                            <Animated.View style={animatedStyles}>
+                                <TouchableOpacity
+                                    style={modalStyle.rootContainer}
+                                    onPress={() => {
+                                        setEnlargedPhotoVisible(false);
+                                        setPhotoInFocus(null);
+                                    }}
+                                >
+                                    <TouchableWithoutFeedback>
+                                        <Image
+                                            source={{ uri: photoInFocus?.publicUrl }}
+                                            style={modalStyle.image}
+                                        />
+                                    </TouchableWithoutFeedback>
+                                </TouchableOpacity>
+                            </Animated.View>
+                        </GestureDetector>
+                    </GestureHandlerRootView>
                 </SafeAreaView>
             </Modal>
         </SafeAreaView>
