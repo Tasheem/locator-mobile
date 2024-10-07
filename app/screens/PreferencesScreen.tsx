b6d4a8103b3f55dbe205119627a8f754a507745e
Tasheem <thargro1@uab.edu>
2024-10-07 11:58:13 -0500

    Add loader for saving on preferences screen

--- app/screens/PreferencesScreen.tsx
+++ app/screens/PreferencesScreen.tsx
@@ -1,19 +1,24 @@
-import { Alert, SafeAreaView, ScrollView } from "react-native"
+import { ActivityIndicator, Alert, SafeAreaView, ScrollView } from "react-native"
 import Preferences from "../components/Preferences"
 import { RootStackParamList } from "../../App"
 import { DrawerScreenProps } from "@react-navigation/drawer"
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PlaceType } from "../models/places";
 import { fetchPlaceTypes } from "../services/places-service";
 import { getUserPreferences, updateUserPreferences } from "../services/user-service";
 import LocatorButton from "../components/LocatorButton";
+import { BRAND_RED } from "../constants/colors";
+import { ScreenContext } from "../utils/context";
 
 
 export default function PreferencesScreen({ route, navigation }: DrawerScreenProps<RootStackParamList, 'Preferences'>) {
+    const { widthRatio } = useContext(ScreenContext);
+
     const [placeTypes, setPlaceTypes] = useState<PlaceType[]>([]);
     const [placeTypesLoading, setPlaceTypesLoading] = useState(false);
     const [originalPreferences, setOriginalPreferences] = useState(new Set<number>());
     const [selectedPreferences, setSelectedPreferences] = useState(new Set<number>());
+    const [saving, setSaving] = useState(false);
 
     useEffect(() => {
         setPlaceTypesLoading(true);
@@ -76,20 +81,39 @@ export default function PreferencesScreen({ route, navigation }: DrawerScreenPro
     return (
         <SafeAreaView>
             <ScrollView>
-                <LocatorButton type="Primary" textValue="Save" handler={() => {
-                    updateUserPreferences(Array.from(selectedPreferences))
-                    .then(response => {
-                        if(response.ok) {
-                            setOriginalPreferences(new Set([...selectedPreferences]));
-                            Alert.alert('Success', 'Your new preferences have been saved.');
-                        } else {
-                            Alert.alert('Error', 'An error occurred while saving your preferences. Please try again later.');
-                        }
-                    })
-                    .catch(() => {
-                        Alert.alert('Error', 'An error occurred while saving your preferences. Please try again later.');
-                    });
-                }} />
+                {
+                    saving ? (
+                        <ActivityIndicator
+                            animating={saving}
+                            color={BRAND_RED}
+                            size={widthRatio > 1.5 ? 'large' : 'small'}
+                        />
+                    ) : (
+                        <LocatorButton
+                            type="Primary"
+                            textValue="Save"
+                            handler={() => {
+                                setSaving(true);
+
+                                updateUserPreferences(Array.from(selectedPreferences))
+                                .then(response => {
+                                    if(response.ok) {
+                                        setOriginalPreferences(new Set([...selectedPreferences]));
+                                        Alert.alert('Success', 'Your new preferences have been saved.');
+                                    } else {
+                                        Alert.alert('Error', 'An error occurred while saving your preferences. Please try again later.');
+                                    }
+                                })
+                                .catch(() => {
+                                    Alert.alert('Error', 'An error occurred while saving your preferences. Please try again later.');
+                                })
+                                .finally(() => {
+                                    setSaving(false);
+                                });
+                            }}
+                        />
+                    )
+                }
                 <Preferences 
                     placeTypes={placeTypes}
                     placeTypesLoading={placeTypesLoading}
